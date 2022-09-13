793b624552151b6ef2e803a67e616b338333626b
LakshmiNarayana353 <112751417+LakshmiNarayana353@users.noreply.github.com>
2022-09-13 16:03:03 +0530

    import java.util.Scanner; class sample{     public static void main(String args[])     {         int a,b;         Scanner sc= new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         b =a%b;         System.out.println(b);     } }

---
+++ Modulus.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class sample{
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner sc= new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        b =a%b;   
+        System.out.println(b);  
+    }
+}

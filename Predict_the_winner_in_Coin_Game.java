082b12f5ba4a0fb6807a06c413b43dd148982339
LakshmiNarayana353 <112751417+LakshmiNarayana353@users.noreply.github.com>
2022-10-27 15:29:19 +0530

    import java.util.Scanner;class sample123{public static void main(String args[]){Scanner sc=new Scanner(System.in);int m,n;m=sc.nextInt();n=sc.nextInt();if(m%2==0 || n%2==0) System.out.println("Player 1");else System.out.println("Player 2");}}

---
+++ Predict_the_winner_in_Coin_Game.java
@@ -0,0 +1,1 @@
+import java.util.Scanner;class sample123{public static void main(String args[]){Scanner sc=new Scanner(System.in);int m,n;m=sc.nextInt();n=sc.nextInt();if(m%2==0 || n%2==0) System.out.println("Player 1");else System.out.println("Player 2");}}

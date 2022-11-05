593d0319c9b768bf48e1b536303bf1e3d0053446
LakshmiNarayana353 <112751417+LakshmiNarayana353@users.noreply.github.com>
2022-11-05 13:45:26 +0530

    import java.util.*; class main{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,s=0;         n=sc.nextInt();         for(i=1;i<n;i++)         { if(n%i==0) s=s+i; }         if(s==n)             System.out.print("True" );         else             System.out.print("False" ); }}

--- Perfect_Number.java
+++ Perfect_Number.java
@@ -1,30 +1,13 @@
-import java.util.Scanner;
-class sma{
-    public static int factorial(int x)
+import java.util.*;
+class main{
+    public static void main(String args[]) 
     {
-        int r,sum=0;
-         r= (int)x/2;
-         for (int i=1;i<x;i++)
-         {
-             if (x%i==0)
-             {
-                 sum=sum+i;
-                 
-             }
-         }
-         return sum;
-        
-    }
-    public static void main(String args[])
-    {
-      Scanner sc= new Scanner(System.in);
-      int n;
-      n = sc.nextInt();
-      int c = factorial(n);
-      if(c==n)
-      System.out.println("True");
-      else
-      System.out.println("False");
-      
-    }
-}
+        Scanner sc=new Scanner(System.in);
+        int n,i,s=0;
+        n=sc.nextInt(); 
+        for(i=1;i<n;i++) 
+        { if(n%i==0) s=s+i; }
+        if(s==n) 
+            System.out.print("True" ); 
+        else
+            System.out.print("False" ); }}

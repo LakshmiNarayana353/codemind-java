5504ffa14ea97f34088c5ff81e18a16ea49efaa3
LakshmiNarayana353 <112751417+LakshmiNarayana353@users.noreply.github.com>
2023-01-21 15:46:56 +0530

    import java.util.*; import java.util.Map.*; class demo{     public static void printOutput(int arr[])     {         int sum=0,count=0;         HashMap<Integer,Integer>hm=new HashMap<>();         for (int i=0;i<arr.length;i++){             hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);         }         for(int i=0;i<arr.length;i++){             if(arr[i]==(int)hm.get(arr[i]))             {                 System.out.print(arr[i]+" ");                 hm.put(arr[i],0);                 count++;             }         }         if(count==0)           System.out.println("-1");     }     public static void main (String[] args)     {         int n,x[];         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)          x[i]=sc.nextInt();         printOutput(x);     } }

---
+++ Elements_same_as_count.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.util.Map.*;
+class demo{
+    public static void printOutput(int arr[])
+    {
+        int sum=0,count=0;
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        for (int i=0;i<arr.length;i++){
+            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==(int)hm.get(arr[i]))
+            {
+                System.out.print(arr[i]+" ");
+                hm.put(arr[i],0);
+                count++;
+            }
+        }
+        if(count==0)
+          System.out.println("-1");
+    }
+    public static void main (String[] args)
+    {
+        int n,x[];
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+         x[i]=sc.nextInt();
+        printOutput(x);
+    }
+}
